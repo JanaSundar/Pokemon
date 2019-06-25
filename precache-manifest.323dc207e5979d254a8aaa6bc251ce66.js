7092abb83c2d1ce6a871bfefbb5ca99a3e779013
Janarthanan <janasundar1997@gmail.com>
2019-06-25 22:42:01 +0530

    Updates

---
+++ precache-manifest.323dc207e5979d254a8aaa6bc251ce66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c686d2a87658d2e7f0fe03216ddb96",
+    "url": "/Pokemon/index.html"
+  },
+  {
+    "revision": "3c79feeeb8733c18fa2b",
+    "url": "/Pokemon/static/css/2.8ad820d0.chunk.css"
+  },
+  {
+    "revision": "57b1016ac515373c72c3",
+    "url": "/Pokemon/static/css/main.59fe301d.chunk.css"
+  },
+  {
+    "revision": "3c79feeeb8733c18fa2b",
+    "url": "/Pokemon/static/js/2.39e9aaf3.chunk.js"
+  },
+  {
+    "revision": "57b1016ac515373c72c3",
+    "url": "/Pokemon/static/js/main.de3a31b4.chunk.js"
+  },
+  {
+    "revision": "b733a247610cfe94ac5e",
+    "url": "/Pokemon/static/js/runtime~main.7b3eed47.js"
+  }
+]);
